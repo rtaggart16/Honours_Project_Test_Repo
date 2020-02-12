2d3aa0e768450d455f17e8185f07e28792996e0c
rtaggart16-test <rosstaggart.dev@gmail.com>
2020-02-12 19:17:19 +0000

    Added HomeController, Index view, and application middleware

---
+++ Honours_Project_Test/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Honours_Project_Test.Controllers
+{
+    /// <summary>
+    /// The default controller of the application
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Main method of the application that displays the home page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
